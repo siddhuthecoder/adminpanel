9a6efbbff03c402fedfdca26438449d9aec1f1d2
Hemu21 <rajva5727@gmail.com>
2024-04-06 23:16:17 +0530

    workshop registered students

--- src/components/workshops/About.jsx
+++ src/components/workshops/About.jsx
@@ -1,23 +1,120 @@
-import React from 'react'
+import React from "react";
 
 const WorkshopAbout = ({ data }) => {
-    return (
-        <>
-            <section className="w-100 d-flex flex-column pe-3">
-                <div className="h3 py-2 text-white">{data.name}</div>
-                <div className="" style={{ textIndent: "20px" }}>
-                <div dangerouslySetInnerHTML={{ __html: data.about }} />
+  return (
+    <>
+      <section className="w-100 d-flex flex-column pe-3">
+        <div className="h3 py-2 text-white">{data.name}</div>
+        <div className="" style={{ textIndent: "20px" }}>
+          <div dangerouslySetInnerHTML={{ __html: data.about }} />
+        </div>
+        <div className="h5 py-2 text-white">structure</div>
+        <div style={{ textIndent: "20px" }}>
+          <div dangerouslySetInnerHTML={{ __html: data.structure }} />
+        </div>
+        <div className="h5 py-2 text-white">Entry Fee</div>
+        <p className="" style={{ textIndent: "20px" }}>
+          {data.entryFee}
+        </p>
+        <div
+          className="table-responsive align-items-center table-striped  mt-4"
+          style={{ height: "auto", width: "1000px", overflow: "" }}
+        >
+          <div
+            className={` d-flex text-center ${"justify-content-between"} align-items-center flex-wrap`}
+          >
+            <div
+              className="d-flex flex-column mx-auto"
+              style={{
+                width: "100%",
+                minWidth: "350px",
+                overflowX: "scroll",
+              }}
+            >
+              <div className="d-flex align-items-center flex-column   justify-content-center">
+                <div
+                  className="d-flex text-center justify-content-around bg-dark text-white align-items-center  mb-2  py-1 shadow"
+                  style={{ width: "100%", minWidth: "950px", height: "50px" }}
+                >
+                  <div className="" style={{ width: "150px" }}>
+                    S.no
+                  </div>
+                  <div
+                    className="d-flex text-center align-items-center"
+                    style={{ minWidth: "250px" }}
+                  >
+                    Name
+                  </div>
+                  <div
+                    className="d-flex align-items-center"
+                    style={{ minWidth: "250px" }}
+                  >
+                    <span className="pe-4">Email ID</span>
+                  </div>
+                  <div
+                    className="d-flex align-items-center"
+                    style={{ minWidth: "200px" }}
+                  >
+                    <span className="pe-4">College</span>
+                  </div>
+                  <div
+                    className="d-flex align-items-center"
+                    style={{ minWidth: "150px" }}
+                  >
+                    College ID
+                  </div>
                 </div>
-                <div className="h5 py-2 text-white">structure</div>
-                <div style={{ textIndent: "20px" }}>
-                <div dangerouslySetInnerHTML={{ __html: data.structure }} />
-                </div>
-                <div className="h5 py-2 text-white">Entry Fee</div>
-                <p className="" style={{ textIndent: "20px" }}>
-                    {data.entryFee}
-                </p>
-            </section>
-        </>
-    )
-}
-export default WorkshopAbout
+                {data?.regStudents?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="d-flex justify-content-around align-items-center hover-effect cursor-pointer  my-1  py-4 shadow"
+                    style={{
+                      width: "100%",
+                      minWidth: "950px",
+                      height: "50px",
+                      cursor: "pointer",
+                      backgroundColor: "black",
+                    }}
+                  >
+                    <div
+                      className="align-items-center"
+                      style={{ minWidth: "150px" }}
+                    >
+                      {index + 1}
+                    </div>
+                    <div
+                      className="d-flex   align-items-center"
+                      style={{ minWidth: "250px" }}
+                    >
+                      <span className="pe-4">{item.name}</span>
+                    </div>
+                    <div
+                      className="d-flex align-items-center"
+                      style={{ minWidth: "250px" }}
+                    >
+                      {item.email}
+                    </div>
+
+                    <div
+                      className="d-flex align-items-center"
+                      style={{ minWidth: "200px" }}
+                    >
+                      {item.college}
+                    </div>
+                    <div
+                      className="d-flex align-items-center"
+                      style={{ minWidth: "300px" }}
+                    >
+                      {item.idNumber}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default WorkshopAbout;
--- src/components/workshops/WorkShopDetails.jsx
+++ src/components/workshops/WorkShopDetails.jsx
@@ -1,113 +1,151 @@
-import { useState, useEffect, useContext } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import axios from 'axios'
+import { useState, useEffect, useContext } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 import { MdKeyboardBackspace } from "react-icons/md";
 import { GrNext } from "react-icons/gr";
-import WorkshopAbout from './About';
-import Instructer from './Instructer';
-import EditWorkShop from './EditWorkShop';
-import DeleteWorkshop from './DeleteWorkshop';
-import { toast } from 'react-toastify';
-import { context } from '../../App';
-
-
-
+import WorkshopAbout from "./About";
+import Instructer from "./Instructer";
+import EditWorkShop from "./EditWorkShop";
+import DeleteWorkshop from "./DeleteWorkshop";
+import { toast } from "react-toastify";
+import { context } from "../../App";
 
 const WorkshopDetails = () => {
+  const [tab, setTab] = useState("About");
+  const { token } = useContext(context);
+  const [workShop, setWorkshop] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
 
-    const [tab, setTab] = useState("About")
-    const {token} = useContext(context)
-    const [workShop, setWorkshop] = useState({})
-    const [loading, setLoading] = useState(false)
-    const { id } = useParams()
-
-
-
-    const fetchData = async () => {
-        setLoading(true);
-        try {
-            const responseData = await axios.get(`${import.meta.env.VITE_API}/workshops/${id}`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": ` Bearer ${token}`,
-                }
-            })
-            setWorkshop(responseData.data)
-        
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const responseData = await axios.get(
+        `${import.meta.env.VITE_API}/workshops/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: ` Bearer ${token}`,
+          },
         }
-        catch (err) {
-            toast.error("Internal Error",{theme:"colored"})
-        }
-        setLoading(false);
+      );
+      setWorkshop(responseData.data);
+    } catch (err) {
+      toast.error("Internal Error", { theme: "colored" });
     }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [token]);
 
-    useEffect(() => {
-        fetchData()
-    }, [token])
+  const goback = () => {
+    window.history.back();
+  };
 
-    const goback = () => {
-        window.history.back()
-    }
-
-    if (loading) {
-        return (
-            <>
-                <div className="text-center">Loading....</div>
-            </>
-        )
-    }
-
+  if (loading) {
     return (
-        <>
+      <>
+        <div className="text-center">Loading....</div>
+      </>
+    );
+  }
 
-            <section className="w-100 d-flex flex-column  border" style=
-                {{
-                    height: "100vh",
-                    overflow: "scroll",
-                    backgroundColor: "rgb(18,18,18)"
-                }}>
-                <div className="w-100 d-flex align-items-center fixed-top justify-content-between shadow" style={{ height: "50px", backgroundColor: "#333333" }}>
-                    <div className="mx-3">
-                        <Link to="#" onClick={goback}>
-                            <MdKeyboardBackspace style={{ fontSize: "30px", color: "#ffffff" }} />
-                        </Link>
-                    </div>
-                    <div className="h2 text-white px-2" >TECKZITE</div>
-                </div>
-                <div className="w-100 mt-5  d-flex align-items-center">
-                    <div className="h3 ms-3 text-white py-3 ">Workshops</div>
-                    <GrNext className=" h4 text-primary" />
-                    <div className="h3 me-1 text-white">{workShop.name}</div>
-                </div>
-                <div className="w-100 row mx-auto">
-                    <div className="col-11 col-md-6 col-lg-4 mx-auto py-4 my-3 d-flex flex-column" style={{
-                        backgroundColor: "black",
-                        maxHeight: "300px"
-                    }}>
-                        <img src={workShop.workshopImg} alt="" className=" mx-auto mt-2" style={{ width: "95%", borderRadius: "4px", height: "100%" }} />
-                        <div className="text-center py-3 h1 " style={{ color: "#006996" }} >{workShop.name}</div>
-                    </div>
-                    <div className="col-11 col-md-6 my-3 py-4 col-lg-7 mx-auto" style={{
-                        backgroundColor: "black",
-                    }}>
-                        <div className="w-100 d-flex align-items-center text-white">
-                            <div className={` px-3 ${tab == "About" ? "tab-active" : ""} `} onClick={() => setTab("About")}>About</div>
-                            <div className={` px-3 ${tab == "Edit" ? "tab-active" : ""} `} onClick={() => setTab("Edit")}>Edit</div>
-                            <div className={` px-3 ${tab == "Instructer" ? "tab-active" : ""} `} onClick={() => setTab("Instructer")}>Instructer</div>
-                            <div className={` px-3 ${tab == "Delete" ? "tab-active" : ""} `} onClick={() => setTab("Delete")}>Delete</div>
-                        </div>
-                        <hr style={{ color: "grey", fontWeight: "700", marginTop: "2px", }} />
-                        <div className="section w-100 " style={{ height: "70vh", overflow: "scroll" }}>
-                            {tab == "About" && <WorkshopAbout data={workShop} />}
-                            {tab == "Instructer" && <Instructer data={workShop} />}
-                            {tab == "Edit" && <EditWorkShop data={workShop} />}
-                            {tab == "Delete" && <DeleteWorkshop />}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
-export default WorkshopDetails
+  return (
+    <>
+      <section
+        className="w-100 d-flex flex-column  border"
+        style={{
+          height: "100vh",
+          overflow: "scroll",
+          backgroundColor: "rgb(18,18,18)",
+        }}
+      >
+        <div
+          className="w-100 d-flex align-items-center fixed-top justify-content-between shadow"
+          style={{ height: "50px", backgroundColor: "#333333" }}
+        >
+          <div className="mx-3">
+            <Link to="#" onClick={goback}>
+              <MdKeyboardBackspace
+                style={{ fontSize: "30px", color: "#ffffff" }}
+              />
+            </Link>
+          </div>
+          <div className="h2 text-white px-2">TECKZITE</div>
+        </div>
+        <div className="w-100 mt-5  d-flex align-items-center">
+          <div className="h3 ms-3 text-white py-3 ">Workshops</div>
+          <GrNext className=" h4 text-primary" />
+          <div className="h3 me-1 text-white">{workShop.name}</div>
+        </div>
+        <div className="w-100 row mx-auto">
+          <div
+            className="col-11 col-md-6 col-lg-4 mx-auto py-4 my-3 d-flex flex-column"
+            style={{
+              backgroundColor: "black",
+              maxHeight: "300px",
+            }}
+          >
+            <img
+              src={workShop.workshopImg}
+              alt=""
+              className=" mx-auto mt-2"
+              style={{ width: "95%", borderRadius: "4px", height: "100%" }}
+            />
+            <div className="text-center py-3 h1 " style={{ color: "#006996" }}>
+              {workShop.name}
+            </div>
+          </div>
+          <div
+            className="col-11 col-md-6 my-3 py-4 col-lg-7 mx-auto"
+            style={{
+              backgroundColor: "black",
+            }}
+          >
+            <div className="w-100 d-flex align-items-center text-white">
+              <div
+                className={` px-3 ${tab == "About" ? "tab-active" : ""} `}
+                onClick={() => setTab("About")}
+              >
+                About
+              </div>
+              <div
+                className={` px-3 ${tab == "Edit" ? "tab-active" : ""} `}
+                onClick={() => setTab("Edit")}
+              >
+                Edit
+              </div>
+              <div
+                className={` px-3 ${tab == "Instructer" ? "tab-active" : ""} `}
+                onClick={() => setTab("Instructer")}
+              >
+                Instructer
+              </div>
+              <div
+                className={` px-3 ${tab == "Delete" ? "tab-active" : ""} `}
+                onClick={() => setTab("Delete")}
+              >
+                Delete
+              </div>
+            </div>
+            <hr
+              style={{ color: "grey", fontWeight: "700", marginTop: "2px" }}
+            />
+            <div
+              className="section w-100 "
+              style={{ height: "70vh", overflow: "scroll" }}
+            >
+              {tab == "About" && <WorkshopAbout data={workShop} />}
+              {tab == "Instructer" && <Instructer data={workShop} />}
+              {tab == "Edit" && <EditWorkShop data={workShop} />}
+              {tab == "Delete" && <DeleteWorkshop />}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default WorkshopDetails;
